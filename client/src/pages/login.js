d54b3a5ebfe3a0e79ea01aabe46e2c306fa322b6
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-02-18 14:04:49 +0700

    fix loi file login.js va register.js

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+//import { Link } from "react-router-dom";
 
 const login = () => {
   /*const initialState = { email: "", password: "" };
@@ -11,41 +11,7 @@ const login = () => {
     setUserData({ ...userData, [name]: value });
   };*/
 
-  return (
-    <div className="auth_page">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  return <div>login</div>;
 };
 
 export default login;

857dec9590dbbb05130a7f7fc188311b6b782ef3
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-02-19 14:18:16 +0700

    Add denpendencies : classname, fontawesomeicon, Add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom'
 import PageRender from './PageRender';
+import Login from './pages/login/Login'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <div className="main">
           <Routes>
+            <Route exact path="/login" element={<Login/>} />
             <Route exact path="/:page" element={<PageRender/>} />
             <Route exact path="/:page/:id" element={<PageRender/>} />
           </Routes>

71399d93cd5cf600e8d541e4c1908b99172a6a41
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-02-23 10:00:54 +0700

    sadfasd
    fasdfas
    
    Co-Authored-By: Le Hoang Minh Tuan <100018096+UncleTuan@users.noreply.github.com>

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>asobale</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;

0ef076fbca51c0d7da5e879a601102245ca71365
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-02-24 20:14:24 +0700

    update file authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,7 +35,7 @@ const authController = {
       const access_token = createAccessToken({ id: newUser._id });
       const refresh_token = createRefreshToken({ id: newUser._id });
 
-      res.cookie("refresh_token", refresh_token, {
+      res.cookie("refreshtoken", refresh_token, {
         httpOnly: true,
         path: "/api/generateAccessToken",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
@@ -58,21 +58,80 @@ const authController = {
 
   login: async (req, res) => {
     try {
-    } catch (error) {
+      const { email, password } = req.body;
+
+      const user = await Users.findOne({ email }).populate(
+        "followers following",
+        "-password"
+      );
+
+      if (!user)
+        return res.status(400).json({ msg: "This email does not exist" });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch)
+        return res.status(400).json({ msg: "Password is incorrect" });
+
+      const access_token = createAccessToken({ id: user._id });
+      const refresh_token = createRefreshToken({ id: user._id });
+
+      res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "/api/generateAccessToken",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      });
+
+      res.json({
+        msg: "Login Success!",
+        access_token,
+        user: {
+          ...user._doc,
+          password: "",
+        },
+      });
+    } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
 
   logout: async (req, res) => {
     try {
-    } catch (error) {
+      res.clearCookie("refreshtoken", {
+        path: "/api/generateAccessToken",
+      });
+      return res.json({ msg: "Logged out!" });
+    } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
 
   generateAccessToken: async (req, res) => {
     try {
-    } catch (error) {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token) return res.status(400).json({ msg: "Please login now" });
+
+      jwt.verify(
+        rf_token,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, result) => {
+          if (err) return res.status(400).json({ msg: "Please login now" });
+
+          const user = await Users.findById(result.id)
+            .select("-password")
+            .populate("followers following", "-password");
+
+          if (!user)
+            return res.status(400).json({ msg: "This does not exist" });
+
+          const access_token = createAccessToken({ id: result.id });
+          res.json({
+            access_token,
+            user,
+          });
+        }
+      );
+    } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
   },
--- models/userModel.js
+++ models/userModel.js
@@ -45,15 +45,9 @@ const userSchema = new mongoose.Schema(
       maxlength: 200,
     },
     website: { type: String, default: "" },
-    followers: [
-        { type: mongoose.Types.ObjectId, ref: "user" }
-    ],
-    following: [
-        { type: mongoose.Types.ObjectId, ref: "user" }
-    ],
-    saved: [
-        { type: mongoose.Types.ObjectId, ref: "user" }
-    ],
+    followers: [{ type: mongoose.Types.ObjectId, ref: "user" }],
+    following: [{ type: mongoose.Types.ObjectId, ref: "user" }],
+    saved: [{ type: mongoose.Types.ObjectId, ref: "user" }],
   },
   {
     timestamps: true,

6608272ac4b056582881205cdf939b1f0a3f5268
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-02-24 17:02:24 +0700

    update continue athentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,39 +1,93 @@
-const Users = require('../models/userModel')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const authController = {
-    register: async (req, res) => {
-        try {
-            const {fullname, username, phone, email, password, gender} = req.body
-        } catch (error) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
+  register: async (req, res) => {
+    try {
+      const { fullname, username, phone, email, password, gender } = req.body;
+      let newUserName = username.toLowerCase().replace(/ /g, "");
 
-    login: async (req, res) => {
-        try {
-            
-        } catch (error) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
+      const user_name = await Users.findOne({ username: newUserName });
+      if (user_name)
+        return res.status(400).json({ msg: "This user name already exists." });
 
-    logout: async (req, res) => {
-        try {
-            
-        } catch (error) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
+      const user_email = await Users.findOne({ email });
+      if (user_email)
+        return res.status(400).json({ msg: "This email already exists." });
 
-    generateAccessToken: async (req, res) => {
-        try {
-            
-        } catch (error) {
-            return res.status(500).json({msg: err.message})
-        }
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Password must be at least 6 characters." });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = new Users({
+        fullname,
+        username: newUserName,
+        phone,
+        email,
+        password: passwordHash,
+        gender,
+      });
+
+      const access_token = createAccessToken({ id: newUser._id });
+      const refresh_token = createRefreshToken({ id: newUser._id });
+
+      res.cookie("refresh_token", refresh_token, {
+        httpOnly: true,
+        path: "/api/generateAccessToken",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      });
+
+      await newUser.save();
+
+      res.json({
+        msg: "Registed Success!",
+        access_token,
+        user: {
+          ...newUser._doc,
+          password: "",
+        },
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
     }
-}
+  },
+
+  login: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  logout: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  generateAccessToken: async (req, res) => {
+    try {
+    } catch (error) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = authController;

fb89aedde7f7a90f411352ecf6f0ab2cf4246b00
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-03-03 19:23:53 +0700

    fix header done

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,16 +1,14 @@
 import React from "react";
-import classNames from "classnames/bind";
-import styles from "../styles/home.css";
+//import classNames from "classnames/bind";
+//import styles from "../styles/home.css";
 import Header from "../components/Header";
 
-
-const cx = classNames.bind(styles);
+//const cx = classNames.bind(styles);
 
 const Home = () => {
   return (
     <div>
       <Header />
-      
     </div>
   );
 };

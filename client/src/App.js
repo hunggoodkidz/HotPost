87ea64a0d8b370adebea3eb6bfb54243a5ded07b
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-03-04 10:51:44 +0700

    help

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PageRender from "./PageRender";
+import PageRender from "./customRouter/PageRender";
+import PrivateRouter from "../customRouter/PrivateRouter";
+
 import Home from "./pages/home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/register";
 import Header from "./components/Header";
 import Alert from "./components/alert/Alert";
@@ -23,12 +25,14 @@ function App() {
       <Alert />
       <input type="checkbox" id="theme" />
       <div className="App">
+        {auth.token && <Header />}
+
         <Routes>
-          {auth.token && <Header />}
-          {console.log(auth.user._id)}
-          <Route  path="/" element={auth.token ? <Home /> : <Login/>} />
-          <Route  path="/:page" element={<PageRender />} />
-          <Route  path="/:page/:id" element={<PageRender />} />
+          <Route path="/" element={auth.token ? <Home /> : <Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <PrivateRouter path="/:page" element={<PageRender />} />
+          <PrivateRouter path="/:page/:id" element={<PageRender />} />
         </Routes>
       </div>
     </Router>

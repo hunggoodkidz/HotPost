26aab76005026d3dc438b7c22b4430b3ff489f44
ThànhCorn <99862284+ThanhCorn@users.noreply.github.com>
2023-03-10 13:06:09 +0700

    User routes Setup Post routes Setup

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,8 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "../routes/auth.js";
-import { register } from "../controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,9 +43,11 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/uses", userRoutes);
+app.use("/post", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;

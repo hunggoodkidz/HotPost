88ae1dcc7f84ba30ba4288c1b738218908478e27
minhtuan241001@gmail.com <minhtuan241001@gmail.com>
2023-03-14 20:59:58 +0700

    up up

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PageRender from "./PageRender";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Notify from './components/notify/Notify';
-
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scense/homePage";
+import LoginPage from "scense/loginPage";
+import ProfilePage from "scense/profilePage";
 
 function App() {
   return (
-    <Router>
-      <Notify />
-      <input type="checkbox" id="theme" />
-      <div className="App">
-        <div className="main">
-          <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route exact path="/:page" element={<PageRender />} />
-              <Route exact path="/:page/:id" element={<PageRender />} />
-          </Routes>
-        </div>
-      </div>
-    </Router> 
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
